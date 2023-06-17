f77308e8533f18e08146e290479b950404055df5
Mura Aitkulov <muramarch@MacBook-Air-Mura.local>
2023-06-18 00:04:46 +0600

    add candidates models, and candidates functionals, and serializers

---
+++ apps/candidates/views.py
@@ -0,0 +1,24 @@
+from apps.candidates.models import Candidate
+from apps.candidates.serializers import CandidateSerializer, CandidateStatisticSerializer
+from rest_framework import viewsets
+from django.db.models import Count
+
+
+class CandidateViewSet(viewsets.ModelViewSet):
+    queryset = Candidate.objects.all()
+    serializer_class = CandidateSerializer
+
+
+class CandidateStatisticView(viewsets.ModelViewSet):
+    serializer_class = CandidateStatisticSerializer
+    lookup_field = 'election_pk'
+
+    def get_queryset(self):
+        election = self.kwargs['election_pk']
+        filtering_by_election = Candidate.objects.filter(election=election)
+        candidate_statistic = filtering_by_election.values('name').annotate(
+            counting=Count('name'),
+            percentage=Count('name')*100/len(filtering_by_election)
+        )
+        return candidate_statistic
+
