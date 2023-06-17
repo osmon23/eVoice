606c07268851b456ba5e7ea2686ccdb9293eb191
Dastan <dastiw1010@gmail.com>
2023-06-17 22:33:49 +0600

    added smtp

--- config/settings.py
+++ config/settings.py
@@ -193,5 +193,13 @@ SPECTACULAR_SETTINGS = {
 }
 
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env_config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env_config('EMAIL_HOST_PASSWORD')
+
+
 with contextlib.suppress(ImportError):
     from .local_settings import *
